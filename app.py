40e13fc64b88fc04cf4b8c8523ff1ebf803abfb7
vishal karhad <vishalkarhad199@gmail.com>
2025-08-09 10:47:19 +0530

    change full python code

--- app.py
+++ app.py
@@ -2,37 +2,31 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Store chat messages in memory (simple demo)
-chat_messages = []
-typing_previews = {}
+messages = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  # तुझा HTML इथे टाक
 
 @socketio.on('send_message')
 def handle_send_message(data):
-    print("Message received:", data)  # Debug
+    messages.append(data)
     emit('new_message', data, broadcast=True)
 
-
 @socketio.on('typing_preview')
-def handle_typing_preview(data):
-    typing_previews[data['sender']] = data['previewText']
+def handle_typing(data):
     emit('typing_update', data, broadcast=True, include_self=False)
 
 @socketio.on('stop_typing')
 def handle_stop_typing(data):
-    if data['sender'] in typing_previews:
-        del typing_previews[data['sender']]
-    emit('typing_update', data, broadcast=True, include_self=False)
+    emit('typing_update', {'sender': data['sender'], 'previewText': ''},
+         broadcast=True, include_self=False)
 
 @socketio.on('request_history')
-def handle_request_history():
-    emit('history', chat_messages)
+def handle_history():
+    emit('history', messages)
 
-if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000)
